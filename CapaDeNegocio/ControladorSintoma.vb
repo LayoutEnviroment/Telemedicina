caa66ac4da94585d2340c6c028f5d18be2a4c077
Leonardo Santos <leonardosantosbran@hotmail.com>
2020-06-17 14:36:32 -0300

    Listado de sintomas creado en el Controlador de Sintomas

---
+++ CapaDeNegocio/ControladorSintoma.vb
@@ -0,0 +1,12 @@
+﻿Imports CapaDeDatos
+
+Public Module ControladorSintoma
+
+    Public Function ListarSintoma(Sintoma As String)
+        Dim p As New ModeloSintoma With {
+            .Nombre = Sintoma
+        }
+
+        Return p.Listar()
+    End Function
+End Module
