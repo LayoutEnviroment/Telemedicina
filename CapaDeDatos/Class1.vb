5bf285141be73c77157ba78871e431eac50804d6
Leonardo Santos <leonardosantosbran@hotmail.com>
2020-06-17 13:25:50 -0300

    Modelo de base de datos creado, Carpeta para modelos creada

--- CapaDeDatos/Class1.vb
+++ CapaDeDatos/Class1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Class1
+﻿Imports System.Data.Odbc
+
+Public MustInherit Class ModeloBaseDeDatos
+
+    Public Driver As String = ""
+    Public Uid As String = ""
+    Public Pwd As String = ""
+    Public Port As String = ""
+    Public DatabaseName As String = ""
+    Public Host As String = ""
+
+    Public Connect As New OdbcConnection("
+        DRIVER=" + Me.Driver +
+        ";UID=" + Me.Uid +
+        ";PWD=" + Me.Pwd +
+        ";PORT=" + Me.Port +
+        ";DATABASE=" + Me.DatabaseName +
+        ";SERVER=" + Me.Host
+    )
+
+    Public Command As New OdbcCommand
+    Public Reader As OdbcDataReader
+
+    Public Sub New()
+        Me.Connect.Open()
+        Me.Command.Connection = Me.Connect
+    End Sub
+
 
 End Class
