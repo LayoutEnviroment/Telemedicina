6f2c8dc5fb7a08db3b7874d2b60b9d16f7fcc743
Leelleon <leonardosantosbran@gmail.com>
2020-08-17 16:13:47 -0300

    Cambios realizados, falta revisar gestor

--- CapaDePresentacion/Frm_Login.vb
+++ CapaDePresentacion/Frm_Login.vb
@@ -5,7 +5,7 @@ Public Class FrmLogin
     Private Sub BtnIngresar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnIngresar.Click
 
         Try
-            If Autentificar(TxtUser.Text, TxtPass.Text) = 1 Then
+            If Autentificar(TxtUser.Text, TxtPass.Text) <> "" Then
                 SetearSesion(TxtUser.Text, TxtPass.Text)
                 Me.Hide()
                 Frm_Menu.Show()
