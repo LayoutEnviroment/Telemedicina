6177352126284513f5d96f16a6c230e4867de802
Leelleon <leonardosantosbran@gmail.com>
2020-09-23 22:21:17 -0300

    Consultas para el medico iniciadas

---
+++ FrmMedico/FrmConsultas.vb
@@ -0,0 +1,13 @@
+﻿Imports CapaDeNegocio
+
+Public Class FrmConsultas
+    Private Sub FrmConsultas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim TablaConsultas As New DataTable
+        Try
+            TablaConsultas.Load(ControladorChatMedico.ObtenerIdFechaMisChat())
+            DgvConsultas.DataSource = TablaConsultas
+        Catch ex As Exception
+            MsgBox("No se pudo obtener las consultas" + ex.ToString)
+        End Try
+    End Sub
+End Class
