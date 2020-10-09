2917a6ed0f341222fe012664440d5de50a906f04
Leelleon <leonardosantosbran@gmail.com>
2020-10-09 19:58:22 -0300

    Nueva enfermedad terminada

--- CapaPresentacionGestion/FrmNuevaEnfermedad.Designer.vb
+++ CapaPresentacionGestion/FrmNuevaEnfermedad.Designer.vb
@@ -39,17 +39,19 @@ Partial Class FrmNuevaEnfermedad
         '
         'TxtNombre
         '
-        Me.TxtNombre.Location = New System.Drawing.Point(117, 60)
+        Me.TxtNombre.Location = New System.Drawing.Point(78, 39)
+        Me.TxtNombre.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtNombre.Name = "TxtNombre"
-        Me.TxtNombre.Size = New System.Drawing.Size(203, 26)
+        Me.TxtNombre.Size = New System.Drawing.Size(137, 20)
         Me.TxtNombre.TabIndex = 0
         '
         'TxtDescripcion
         '
-        Me.TxtDescripcion.Location = New System.Drawing.Point(117, 162)
+        Me.TxtDescripcion.Location = New System.Drawing.Point(78, 105)
+        Me.TxtDescripcion.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtDescripcion.Multiline = True
         Me.TxtDescripcion.Name = "TxtDescripcion"
-        Me.TxtDescripcion.Size = New System.Drawing.Size(203, 26)
+        Me.TxtDescripcion.Size = New System.Drawing.Size(137, 18)
         Me.TxtDescripcion.TabIndex = 1
         '
         'CmbPrioridad
@@ -57,17 +59,19 @@ Partial Class FrmNuevaEnfermedad
         Me.CmbPrioridad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.CmbPrioridad.FormattingEnabled = True
         Me.CmbPrioridad.Items.AddRange(New Object() {"ALTA", "MEDIA", "BAJA"})
-        Me.CmbPrioridad.Location = New System.Drawing.Point(117, 381)
+        Me.CmbPrioridad.Location = New System.Drawing.Point(78, 248)
+        Me.CmbPrioridad.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.CmbPrioridad.Name = "CmbPrioridad"
-        Me.CmbPrioridad.Size = New System.Drawing.Size(203, 28)
+        Me.CmbPrioridad.Size = New System.Drawing.Size(137, 21)
         Me.CmbPrioridad.TabIndex = 2
         '
         'LblNombre
         '
         Me.LblNombre.AutoSize = True
-        Me.LblNombre.Location = New System.Drawing.Point(29, 26)
+        Me.LblNombre.Location = New System.Drawing.Point(19, 17)
+        Me.LblNombre.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.LblNombre.Name = "LblNombre"
-        Me.LblNombre.Size = New System.Drawing.Size(65, 20)
+        Me.LblNombre.Size = New System.Drawing.Size(44, 13)
         Me.LblNombre.TabIndex = 3
         Me.LblNombre.Text = "Nombre"
         '
@@ -76,17 +80,19 @@ Partial Class FrmNuevaEnfermedad
         Me.CmbSintomas.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
         Me.CmbSintomas.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.CmbSintomas.FormattingEnabled = True
-        Me.CmbSintomas.Location = New System.Drawing.Point(381, 58)
+        Me.CmbSintomas.Location = New System.Drawing.Point(254, 38)
+        Me.CmbSintomas.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.CmbSintomas.Name = "CmbSintomas"
-        Me.CmbSintomas.Size = New System.Drawing.Size(193, 28)
+        Me.CmbSintomas.Size = New System.Drawing.Size(130, 21)
         Me.CmbSintomas.TabIndex = 4
         '
         'LstSintomasSeleccionados
         '
         Me.LstSintomasSeleccionados.HideSelection = False
-        Me.LstSintomasSeleccionados.Location = New System.Drawing.Point(381, 117)
+        Me.LstSintomasSeleccionados.Location = New System.Drawing.Point(254, 76)
+        Me.LstSintomasSeleccionados.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.LstSintomasSeleccionados.Name = "LstSintomasSeleccionados"
-        Me.LstSintomasSeleccionados.Size = New System.Drawing.Size(193, 292)
+        Me.LstSintomasSeleccionados.Size = New System.Drawing.Size(130, 191)
         Me.LstSintomasSeleccionados.TabIndex = 5
         Me.LstSintomasSeleccionados.UseCompatibleStateImageBehavior = False
         Me.LstSintomasSeleccionados.View = System.Windows.Forms.View.List
@@ -94,53 +100,60 @@ Partial Class FrmNuevaEnfermedad
         'LblDescripcion
         '
         Me.LblDescripcion.AutoSize = True
-        Me.LblDescripcion.Location = New System.Drawing.Point(23, 139)
+        Me.LblDescripcion.Location = New System.Drawing.Point(15, 90)
+        Me.LblDescripcion.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.LblDescripcion.Name = "LblDescripcion"
-        Me.LblDescripcion.Size = New System.Drawing.Size(92, 20)
+        Me.LblDescripcion.Size = New System.Drawing.Size(63, 13)
         Me.LblDescripcion.TabIndex = 6
         Me.LblDescripcion.Text = "Descripcion"
         '
         'LblPrioridad
         '
         Me.LblPrioridad.AutoSize = True
-        Me.LblPrioridad.Location = New System.Drawing.Point(23, 333)
+        Me.LblPrioridad.Location = New System.Drawing.Point(15, 216)
+        Me.LblPrioridad.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.LblPrioridad.Name = "LblPrioridad"
-        Me.LblPrioridad.Size = New System.Drawing.Size(71, 20)
+        Me.LblPrioridad.Size = New System.Drawing.Size(48, 13)
         Me.LblPrioridad.TabIndex = 7
         Me.LblPrioridad.Text = "Prioridad"
         '
         'LblSintomas
         '
         Me.LblSintomas.AutoSize = True
-        Me.LblSintomas.Location = New System.Drawing.Point(436, 26)
+        Me.LblSintomas.Location = New System.Drawing.Point(291, 17)
+        Me.LblSintomas.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.LblSintomas.Name = "LblSintomas"
-        Me.LblSintomas.Size = New System.Drawing.Size(76, 20)
+        Me.LblSintomas.Size = New System.Drawing.Size(50, 13)
         Me.LblSintomas.TabIndex = 8
         Me.LblSintomas.Text = "Sintomas"
         '
         'BtnCrear
         '
-        Me.BtnCrear.Location = New System.Drawing.Point(691, 45)
+        Me.BtnCrear.Enabled = False
+        Me.BtnCrear.Location = New System.Drawing.Point(461, 29)
+        Me.BtnCrear.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnCrear.Name = "BtnCrear"
-        Me.BtnCrear.Size = New System.Drawing.Size(131, 41)
+        Me.BtnCrear.Size = New System.Drawing.Size(87, 27)
         Me.BtnCrear.TabIndex = 9
         Me.BtnCrear.Text = "Crear"
         Me.BtnCrear.UseVisualStyleBackColor = True
         '
         'BtnLimpiar
         '
-        Me.BtnLimpiar.Location = New System.Drawing.Point(691, 139)
+        Me.BtnLimpiar.Location = New System.Drawing.Point(461, 90)
+        Me.BtnLimpiar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnLimpiar.Name = "BtnLimpiar"
-        Me.BtnLimpiar.Size = New System.Drawing.Size(131, 49)
+        Me.BtnLimpiar.Size = New System.Drawing.Size(87, 32)
         Me.BtnLimpiar.TabIndex = 10
         Me.BtnLimpiar.Text = "Limpiar"
         Me.BtnLimpiar.UseVisualStyleBackColor = True
         '
         'BtnVolver
         '
-        Me.BtnVolver.Location = New System.Drawing.Point(691, 237)
+        Me.BtnVolver.Location = New System.Drawing.Point(461, 154)
+        Me.BtnVolver.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnVolver.Name = "BtnVolver"
-        Me.BtnVolver.Size = New System.Drawing.Size(131, 42)
+        Me.BtnVolver.Size = New System.Drawing.Size(87, 27)
         Me.BtnVolver.TabIndex = 11
         Me.BtnVolver.Text = "Volver"
         Me.BtnVolver.UseVisualStyleBackColor = True
@@ -148,16 +161,17 @@ Partial Class FrmNuevaEnfermedad
         'LblDisponible
         '
         Me.LblDisponible.AutoSize = True
-        Me.LblDisponible.Location = New System.Drawing.Point(154, 25)
+        Me.LblDisponible.Location = New System.Drawing.Point(103, 16)
+        Me.LblDisponible.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.LblDisponible.Name = "LblDisponible"
-        Me.LblDisponible.Size = New System.Drawing.Size(0, 20)
+        Me.LblDisponible.Size = New System.Drawing.Size(0, 13)
         Me.LblDisponible.TabIndex = 12
         '
         'FrmNuevaEnfermedad
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(939, 482)
+        Me.ClientSize = New System.Drawing.Size(664, 318)
         Me.Controls.Add(Me.LblDisponible)
         Me.Controls.Add(Me.BtnVolver)
         Me.Controls.Add(Me.BtnLimpiar)
@@ -171,6 +185,7 @@ Partial Class FrmNuevaEnfermedad
         Me.Controls.Add(Me.CmbPrioridad)
         Me.Controls.Add(Me.TxtDescripcion)
         Me.Controls.Add(Me.TxtNombre)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FrmNuevaEnfermedad"
         Me.Text = "FrmNuevaEnfermedad"
         Me.ResumeLayout(False)
--- CapaPresentacionGestion/FrmNuevaEnfermedad.vb
+++ CapaPresentacionGestion/FrmNuevaEnfermedad.vb
@@ -3,7 +3,7 @@
 Public Class FrmNuevaEnfermedad
 
     Public ListaSintomas As New List(Of String)
-    Public Nombre, Descripcion, Prioridad As Boolean
+    Public Nombre, Descripcion, Prioridad, Sintomas As Boolean
 
     Private Sub FrmNuevaEnfermedad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CargarSintomas()
@@ -28,8 +28,10 @@ Public Class FrmNuevaEnfermedad
 
     Private Sub CmbSintomas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbSintomas.SelectedIndexChanged
         ListaSintomas.Add(CmbSintomas.SelectedItem.ToString())
+        Prioridad = True
         CargarListado()
-        HabilitarCreacion()
+
+
     End Sub
 
     Private Sub CargarListado()
@@ -37,6 +39,7 @@ Public Class FrmNuevaEnfermedad
         For Each sintoma In ListaSintomas
             LstSintomasSeleccionados.Items.Add(sintoma)
         Next
+        VerificarCantidad()
 
     End Sub
 
@@ -51,7 +54,16 @@ Public Class FrmNuevaEnfermedad
         For x = 0 To LstSintomasSeleccionados.Items.Count - 1
             ListaSintomas.Add(LstSintomasSeleccionados.Items(x).Text)
         Next
+        VerificarCantidad()
+    End Sub
 
+    Private Sub VerificarCantidad()
+        If LstSintomasSeleccionados.Items.Count() > 0 Then
+            Sintomas = True
+        Else
+            Sintomas = False
+        End If
+        HabilitarCreacion()
     End Sub
 
     Private Sub BtnCrear_Click(sender As Object, e As EventArgs) Handles BtnCrear.Click
@@ -60,9 +72,12 @@ Public Class FrmNuevaEnfermedad
                                                   TxtDescripcion.Text,
                                                   CmbPrioridad.SelectedItem.ToString,
                                                   ListaSintomas)
+            MsgBox("Enfermedad creada exitosamente", MsgBoxStyle.Information)
+            Limpiar()
         Catch ex As Exception
             MsgBox(ex.ToString + "No se pudo ingresar la enfermedad")
         End Try
+
     End Sub
 
     Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles BtnLimpiar.Click
@@ -76,19 +91,18 @@ Public Class FrmNuevaEnfermedad
     End Sub
 
     Private Sub TxtDescripcion_TextChanged(sender As Object, e As EventArgs) Handles TxtDescripcion.TextChanged
+        If TxtDescripcion.Text <> "" Then
+            Descripcion = True
+        Else
+            Descripcion = False
+        End If
+
         HabilitarCreacion()
     End Sub
 
-    Private Sub Limpiar()
-        TxtNombre.Clear()
-        TxtDescripcion.Clear()
-        LstSintomasSeleccionados.Clear()
-        ListaSintomas.Clear()
-    End Sub
-
     Private Sub TxtNombre_TextChanged(sender As Object, e As EventArgs) Handles TxtNombre.TextChanged
-        If ControladorEnfermedad.ObtenerExistencia(TxtNombre.Text) Then
-            LblDisponible.Text = "Enfermedad existente"
+        If ControladorEnfermedad.ObtenerExistencia(TxtNombre.Text) Or TxtNombre.Text = "" Then
+            LblDisponible.Text = "Enfermedad existente o campo vacio"
             Nombre = False
         Else
             LblDisponible.Text = "Nombre disponible"
@@ -104,6 +118,13 @@ Public Class FrmNuevaEnfermedad
             BtnCrear.Enabled = False
         End If
 
+    End Sub
+
+    Private Sub Limpiar()
+        TxtNombre.Clear()
+        TxtDescripcion.Clear()
+        LstSintomasSeleccionados.Clear()
+        ListaSintomas.Clear()
     End Sub
 
 End Class
