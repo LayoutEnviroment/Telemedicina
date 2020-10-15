e8826102785655b06fdfc564808d40d06d4d90bd
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-15 02:35:31 -0300

    se crean pruebas del controlador chat, revisar el TestAceptarSolicitud y TestBuscarMensajeNuevoConId que estan dando error

--- UnitTest/TestControladorChat.vb
+++ UnitTest/TestControladorChat.vb
@@ -6,27 +6,23 @@ Imports CapaDeDatos
 
     <TestMethod()> Public Sub TestEnviarSolicitud()
         Dim resultado As Boolean
+
         Try
-            Dim c As New ModeloChat("45556667", "VzD91g32s") With {
-            .Cedula = ControladorSesion.Cedula = "45556667",
-            .IdDiagnostico = 1
-            }
+            ControladorChat.EnviarSolicitud("1")
             resultado = True
 
         Catch ex As Exception
             resultado = False
 
         End Try
-        Assert.IsTrue(resultado)
+
 
     End Sub
 
-    'Algo no esta del todo bien
     <TestMethod()> Public Sub TestBuscarSolicitud()
         Dim TablaEsperada As New DataTable
+        Dim c As New ModeloChat("12223334", "yKI9SAAB5")
         Try
-            Dim c As New ModeloChat("12223334", "yKI9SAAB5")
-
             Assert.AreSame(ControladorChat.BuscarSolicitud.GetType(), TablaEsperada.GetType())
 
         Catch ex As Exception
@@ -38,8 +34,6 @@ Imports CapaDeDatos
     <TestMethod()> Public Sub TestAceptarSolicitud()
         Dim resultado As Boolean
         Try
-            Dim c As New ModeloChat("12223334", "yKI9SAAB5")
-
             ControladorChat.AceptarSolicitud("2", "45556667", "doctor", "test")
             resultado = True
         Catch ex As Exception
@@ -49,6 +43,77 @@ Imports CapaDeDatos
         Assert.IsTrue(resultado)
     End Sub
 
+    <TestMethod()> Public Sub TestEnviarMensajePaciente()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.EnviarMensajePaciente(2, "haré cualquiera", "12223334")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+    End Sub
 
+    <TestMethod()> Public Sub TestEnviarMensajeMedico()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.EnviarMensajePaciente(2, "como el tester", "45556667")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+    End Sub
+    <TestMethod()> Public Sub TestBuscarMensajesNuevoConId()
+        Dim TablaEsperada As New DataTable
+        Assert.AreSame(ControladorChat.BuscarMensajesNuevo.GetType(), TablaEsperada.GetType())
 
+    End Sub
+
+    '<TestMethod()> Public Sub TestBuscarMensajesNuevo()
+    '    Dim TablaEsperada As New DataTable
+    '    Assert.AreSame(ControladorChat.BuscarMensajesNuevo.GetType(), TablaEsperada.GetType())
+
+    'End Sub
+
+    <TestMethod()> Public Sub TestMarcarComoLeido()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.MarcarComoLeido(1)
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+        Assert.IsTrue(resultado)
+    End Sub
+    <TestMethod()> Public Sub TestFinalizarChatPaciente()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.FinalizarChatPaciente(2, "12223334")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+
+    End Sub
+
+    <TestMethod()> Public Sub TestFinalizarChat()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.FinalizarChat(2)
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+
+    End Sub
+
+    <TestMethod()> Public Sub TestFinalizarChatMedico()
+        Dim resultado As Boolean
+        Try
+            ControladorChat.FinalizarChatMedico(2, "45556667")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+
+    End Sub
 End Class
