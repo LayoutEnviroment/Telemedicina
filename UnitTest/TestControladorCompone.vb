f6668b5d6f8eb7033e89c8a715da4ae73af71c60
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-18 12:29:10 -0300

    se comienzan las pruebas controladorcompone

---
+++ UnitTest/TestControladorCompone.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports CapaDeNegocio
+
+<TestClass()> Public Class TestControladorCompone
+
+    <TestMethod()> Public Sub TestEnfermedadesPosibles()
+    End Sub
+
+End Class
