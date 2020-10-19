ae0d15d283517a6da7665c085ece598d259d1e5f
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-19 00:18:35 -0300

    se creo la estructura para comenzar las pruebas unitarias del ControladorEnfermedad

---
+++ UnitTest/TestControladorEnfermedad.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class TestControladorEnfermedad
+
+    '<TestMethod()> Public Sub TestListarEnfermedad()
+
+    'End Sub
+
+    '<TestMethod()> Public Sub TestListarNombreEnfermedadesInactiva()
+
+
+    'End Sub
+    '<TestMethod()> Public Sub TestListarNombreEnfermedadesActivas()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestCrearEnfermedad()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestActivarEnfermedad()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestEstaInactivo()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestCambiarEnfermedad()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestCambiarEnfermedadYSintomas()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestEliminarEnfermedad()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestObtenerId()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestObtenerPrioridad()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestObtenerDescripcion()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestObtenerTodo()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestObtenerExistencia()
+
+    'End Sub
+    '<TestMethod()> Public Sub TestReactivarEnfermedad()
+
+    'End Sub
+
+End Class
