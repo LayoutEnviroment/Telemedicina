544964ae5d2c87200ad9c3337af72e898d4efde7
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-20 20:15:26 -0300

    para traer cambios de rama_gestor

--- UnitTest/TestControladorGenera.vb
+++ UnitTest/TestControladorGenera.vb
@@ -1,9 +1,44 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports CapaDeNegocio
 
-<TestClass()> Public Class UnitTest1
+<TestClass()> Public Class TestControladorGenera
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestObtenerIdEnfermedadGenerada()
+
+        Dim resultado As Boolean
+        Try
+            ControladorGenera.ObtenerIdEnfermedadGenerada()
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+        Assert.IsTrue(resultado)
+
     End Sub
+
+    <TestMethod()> Public Sub TestObtenerNombreEnfermedad()
+        Dim resultado As Boolean
+        Try
+            ControladorGenera.ObtenerNombreEnfermedad("2")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+        Assert.IsTrue(resultado)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestObtenerNombreSintomas()
+        Dim resultado As Boolean
+        Try
+            ControladorGenera.ObtenerNombreSintomas("1")
+            resultado = True
+        Catch ex As Exception
+            resultado = False
+        End Try
+        Assert.IsTrue(resultado)
+    End Sub
+
 
 End Class
