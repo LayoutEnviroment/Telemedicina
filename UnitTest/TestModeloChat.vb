fa286717dd81c6be66de588a7df7854f69800799
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-24 17:51:31 -0300

    test modeloChat finalizado

--- UnitTest/TestModeloChat.vb
+++ UnitTest/TestModeloChat.vb
@@ -33,16 +33,73 @@ Imports CapaDeDatos
         Assert.IsTrue(consultaOk)
     End Sub
     <TestMethod()> Public Sub TestAceptarSolicitud()
+        Dim c As New ModeloChat("22223334", "Medico123") With {
+            .Cedula = "22223334",
+            .CiPaciente = "1111",
+            .IdDiagnostico = "4"
+        }
+        Dim insertadoOk As Boolean
+        Try
+            c.AceptarSolicitud()
+            insertadoOk = True
+        Catch ex As Exception
+            insertadoOk = False
+        End Try
+
+        Assert.IsTrue(insertadoOk)
+
 
     End Sub
     <TestMethod()> Public Sub TestEnviarMensajePaciente()
+        Dim c As New ModeloChat("1111", "Todes123") With {
+            .Cedula = "1111",
+            .CiMedico = "22223334",
+            .IdDiagnostico = "4",
+            .CiPaciente = "1111",
+            .Mensaje = "Estoy todito tomado Doc"
+        }
+        Dim insertadoOk As Boolean
+        Try
+            c.AceptarSolicitud()
+            insertadoOk = True
+        Catch ex As Exception
+            insertadoOk = False
+        End Try
+
+        Assert.IsTrue(insertadoOk)
 
     End Sub
+    'consultar por que no guarda el mensaje
     <TestMethod()> Public Sub TestEnviarMensajeMedico()
+        Dim insertadoOk As Boolean
+        Try
+            Dim c As New ModeloChat("22223334", "Medico123") With {
+            .Cedula = "22223334",
+            .CiMedico = "22223334",
+            .IdDiagnostico = "4",
+            .CiPaciente = "1111",
+            .Mensaje = "Nunca estuve tan de acuerdo"
+        }
+            c.AceptarSolicitud()
+            insertadoOk = True
+        Catch ex As Exception
+            insertadoOk = False
+        End Try
 
+        Assert.IsTrue(insertadoOk)
     End Sub
     <TestMethod()> Public Sub TestBuscarMensajesNuevos()
+        Dim tablaEsperada As New DataTable
+        Dim c As New ModeloChat("1111", "Todes123") With {
+            .IdDiagnostico = "4",
+            .Cedula = "1111"}
 
+        Try
+            Assert.AreSame(c.BuscarMensajesNuevos.GetType(), tablaEsperada.GetType())
+
+        Catch ex As Exception
+            MsgBox("No comprobe el dato mandado")
+        End Try
 
     End Sub
     <TestMethod()> Public Sub TestMarcarComoLeido()
