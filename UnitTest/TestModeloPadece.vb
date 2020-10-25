2041138ac6b0e7221451da2f7465384c15cdb0f1
Eduardo Fabre Toledo <egufabre@hotmail.com>
2020-10-25 19:51:39 -0300

    test modeloPadece finalizado

--- UnitTest/TestModeloPadece.vb
+++ UnitTest/TestModeloPadece.vb
@@ -1,9 +1,29 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports CapaDeDatos
 
 <TestClass()> Public Class TestModeloPadece
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestGuardarPadecimientos()
+        Dim resultado As Boolean
+        Dim listaPrueba As New List(Of String) From {
+            "Sintoma de prueba"
+            }
+        Dim p As New ModeloPadece("11112223", "Admin123") With {
+            .ListadoSintomas = listaPrueba,
+            .Cedula = "1111"
+        }
+
+        Try
+            p.GuardarPadecimientos()
+            resultado = True
+
+        Catch ex As Exception
+            resultado = False
+
+        End Try
+
+        Assert.IsTrue(resultado)
     End Sub
 
 End Class
